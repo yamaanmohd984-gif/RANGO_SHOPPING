2f8830f7eb1b3d5591aa7e65749cdd257111d5d4
yamaanmohd984-gif <yamaanmohd984@gmail.com>
2026-07-28 10:41:28 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -2,42 +2,22 @@
 // RANGO SHOPPING JAVASCRIPT
 // =================================
 
-
 // Mobile menu future support
-
 document.addEventListener("DOMContentLoaded", function(){
-
     console.log("Rango Shopping Loaded Successfully");
-
 });
 
-
-
 // Wishlist button animation
-
 function addWishlist(){
-
     alert("Product added to your wishlist!");
-
 }
 
-
-
 // Smooth scrolling
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-
     anchor.addEventListener("click", function(e){
-
         e.preventDefault();
-
-        document.querySelector(this.getAttribute("href"))
-        .scrollIntoView({
-
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
             behavior:"smooth"
-
         });
-
     });
-
 });
